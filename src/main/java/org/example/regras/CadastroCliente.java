8f65bb20048ea1aa73c36b66f21a965739ed53fe
Thalia-guedes <thalia.guedes@zup.com.br>
2023-08-14 19:39:17 -0300

    subindo novas funcoes

--- src/main/java/org/example/regras/CadastroCliente.java
+++ src/main/java/org/example/regras/CadastroCliente.java
@@ -10,12 +10,35 @@ public class CadastroCliente {
         return clientes;
     }
     public Cliente pesquisarPorCpf(String cpf){
-        int tamanho = clientes.size();
         for (Cliente cliente : clientes) {
             if (cliente.getCpf().equals(cpf)) {
                 return cliente;
             }
         }
         return null;
+    }
+    public Cliente adicionar(Cliente cliente){
+        if (!cliente.getEmail().contains("@")){
+            throw new IllegalArgumentException("Email sem @.");
+        }
+        Cliente clienteComCpf = pesquisarPorCpf(cliente.getCpf());
+        if (clienteComCpf != null){
+            throw new IllegalArgumentException("CPF ja existente");
+        }
+        clientes.add(cliente);
+        return cliente;
+    }
+
+    public Cliente remover(String cpf){
+        Cliente cliente = pesquisarPorCpf(cpf);
+        if (cliente != null){
+            clientes.remove(cliente);
+        }
+        return cliente;
+    }
+
+    public Cliente atualizar(Cliente cliente){
+        remover(cliente.getCpf());
+        return adicionar(cliente);
     }
 }
